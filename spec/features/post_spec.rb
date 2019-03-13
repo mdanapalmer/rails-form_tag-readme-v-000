285945053a35f7ed40fe5f52cb1e830785c1736d
Dana Palmer <roiai4@hotmail.com>
2019-03-13 04:40:59 +0000

    Automatically backed up by Learn

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -4,10 +4,14 @@ describe 'new post' do
   it 'ensures that the form route works with the /new action' do
     visit new_post_path
     expect(page.status_code).to eq(200)
+  end
+end
     
   it 'renders HTML in the /new template' do
     visit new_post_path
     expect(page).to have_content('Post Form')
+  end
+end
 
   it "displays a new post form that redirects to the index page, which then contains the submitted post's title and description" do
     visit new_post_path
@@ -21,5 +25,4 @@ describe 'new post' do
     expect(page).to have_content('My post description')
 
   end
-end
 end
